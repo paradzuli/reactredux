e0c96359986c892ef285b5a358c44efd32c9d1c3
Administrator <Administrator@SIMSDEV>
2017-08-21 11:58:48 +0800

    Added build scripts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-﻿import 'babel-polyfill';
+﻿/*eslint-disable import/default*/
+import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
 import configureStore from './store/configureStore';
